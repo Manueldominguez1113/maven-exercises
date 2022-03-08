3b0e491c6a5e3a697416872b4cf3ee1c986b02a5
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-03-08 16:06:56 -0600

    updated code

--- src/main/java/input.java
+++ src/main/java/input.java
@@ -2,12 +2,16 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 public class input {
 
-    public static void run(int input) {
-        System.out.println(input + " hm.. this is a number!");
-    }
+
+
+
 
     public static void run(String input) {
-        System.out.println(input + " hm.. this is a String!");
+        if(StringUtils.isNumeric(input)){
+            System.out.println(input + " hm.. this is a number!");
+        } else{
+            System.out.println(input + " hm.. this is a String!");
+        }
         System.out.println("FlipCase!: " + StringUtils.swapCase(input));
         System.out.println("Reversed!: "+StringUtils.reverse(input));
 }
@@ -15,8 +19,8 @@ public class input {
 
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.print("enter something");
-        String input = scanner.next();
+        System.out.print("enter something: ");
+        String input = (scanner.next());
         run(input);
 
     }
